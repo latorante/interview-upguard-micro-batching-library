b4bc717beb21f1d352d12a1c565317c2e2dcc3d9
Martin Picha <martin@latorante.name>
2024-10-29 06:51:36 +1000

    Change name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "MicroBatching",
+    name: "micro-batching",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
